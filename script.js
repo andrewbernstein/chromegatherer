0341801b4619cf3842233a5349ea57179713b085
cloudofstrife <cloudofstrife@gmail.com>
2013-09-21 18:19:24 -0700

    Removing older code from script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
-// If the letter 'g' is found in the tab's URL...
+    //if we're at a gatherer page
 	if (tab.url.indexOf('gatherer.wizards.com') > -1) {
-    	// ... show the page action.
+    	// show the handy button
     	chrome.pageAction.show(tabId);
-    	//tab.executeScript('injecter.js');
-    	//details = new InjectDetails('script.js');
-    	//chrome.tabs.executeScript(tabId, details);
+        //when we click on the the handy button, run some scripts
     	chrome.pageAction.onClicked.addListener(function(tab) {
-    		chrome.tabs.executeScript(null, {file: "jquery.js"});
-    		chrome.tabs.executeScript(null, {file: "injecter.js"});
+            //add jquery because for some reason wizards doesn't
+    		chrome.tabs.executeScript(null, { file: "jquery.js" });
+            //run the injector to actually strip the things
+            //TODO: Get rid of injecter.js and do it all in here?
+    		chrome.tabs.executeScript(null, { file: "injecter.js" });
     	})
   	}
 };
