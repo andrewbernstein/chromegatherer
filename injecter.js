ed56f9882ddcf4acb01546282e529f57beb12d58
cloudofstrife <cloudofstrife@gmail.com>
2013-09-21 18:00:59 -0700

    Initial, functional but probably ugly commit

---
+++ injecter.js
@@ -0,0 +1,7 @@
+$('div').not('.checklist').hide();
+$('body').append($('.checklist')[0]);
+$('.artist').hide();
+$('.set').hide();
+$('.headerRow').remove();
+$('form').remove();
+$('head').remove();
